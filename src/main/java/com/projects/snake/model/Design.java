d22b52e02a808b813894d5989ec50efbfe48248a
Gaya <yarongaya@gmail.com>
2022-02-24 20:50:18 +0200

    first commit

---
+++ src/main/java/com/projects/snake/model/Design.java
@@ -0,0 +1,24 @@
+package com.projects.snake.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Design {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@NotNull
+	private String borderColor;
+	@NotNull
+	private String snakeColor;
+	@NotNull
+	private String foodColor;
+}
