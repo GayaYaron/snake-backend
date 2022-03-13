4fa90da480cf79fc2857513ec1f35a1a5debbb9d
Gaya <yarongaya@gmail.com>
2022-03-13 16:02:54 +0200

    handle not enough coins

--- src/main/java/com/projects/snake/controller/SnakeExceptionHandler.java
+++ src/main/java/com/projects/snake/controller/SnakeExceptionHandler.java
@@ -11,6 +11,7 @@ import com.projects.snake.exception.AlreadyPurchasedException;
 import com.projects.snake.exception.LoginFailedException;
 import com.projects.snake.exception.NoColorException;
 import com.projects.snake.exception.NoDefaultDesignException;
+import com.projects.snake.exception.NotEnoughCoinsException;
 import com.projects.snake.exception.util.ErrorCode;
 
 @ControllerAdvice
@@ -22,23 +23,28 @@ public class SnakeExceptionHandler {
 	public ErrorDetail alreadyPurchased(AlreadyPurchasedException e) {
 		return new ErrorDetail(HttpStatus.CONFLICT.value(), ErrorCode.ALREADY_PURCHASED.getCode(), e.getMessage());
 	}
-	
+
 	@ExceptionHandler(LoginFailedException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	public ErrorDetail loginFailed(LoginFailedException e) {
 		return new ErrorDetail(HttpStatus.UNAUTHORIZED.value(), ErrorCode.LOGIN_FAILED.getCode(), e.getMessage());
 	}
-	
+
 	@ExceptionHandler(NoColorException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorDetail noColor(NoColorException e) {
 		return new ErrorDetail(HttpStatus.NOT_FOUND.value(), ErrorCode.NO_COLOR.getCode(), e.getMessage());
 	}
-	
+
 	@ExceptionHandler(NoDefaultDesignException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorDetail noDefaultDesign(NoDefaultDesignException e) {
 		return new ErrorDetail(HttpStatus.NOT_FOUND.value(), ErrorCode.NO_DEFAULT.getCode(), e.getMessage());
 	}
-	
+
+	@ExceptionHandler(NotEnoughCoinsException.class)
+	@ResponseStatus(HttpStatus.CONFLICT)
+	public ErrorDetail notEnoughCoins(NotEnoughCoinsException e) {
+		return new ErrorDetail(HttpStatus.CONFLICT.value(), ErrorCode.NOT_ENOUGH_COINS.getCode(), e.getMessage());
+	}
 }
